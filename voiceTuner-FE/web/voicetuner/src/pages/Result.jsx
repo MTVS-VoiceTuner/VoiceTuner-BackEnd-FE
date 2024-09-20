a8ada55803c7973f8665e7e9e3947d419b6b0a0c
JungHyeonmin <2ee42a45@gmail.com>
2024-09-20 14:57:04 +0900

    주석 추가

--- voiceTuner-FE/web/voicetuner/src/pages/Result.jsx
+++ voiceTuner-FE/web/voicetuner/src/pages/Result.jsx
@@ -18,14 +18,16 @@ const Result = () => {
     navigate(`/solution/${tag}`); // 해당 tag를 URL에 포함하여 페이지 이동
   };
 
+  // 아코디언 라이브러리 설치 후 사용 
   return (
-    <Accordion defaultActiveKey="0">
-      {resultList.map((item, index) => (
-        <Accordion.Item key={item.id} eventKey={`${index}`}>
-          <Accordion.Header>{item.title}</Accordion.Header>
-          <Accordion.Body>
-            {item.body}
-            <button onClick={() => onClickHandler(item.tag)}>solution</button>
+    <Accordion defaultActiveKey="0"> {/*첫번째 아코디언이 열려있는지 결정 0 : true, 1 : false */}
+    {/* resultList 배열의 요소를 순회하면서 렌더링한다.*/}
+      {resultList.map((item, index) => (  
+        <Accordion.Item key={item.id} eventKey={`${index}`}> {/*각 아이템에 고유한 key값을 부여-id*/}
+          <Accordion.Header>{item.title}</Accordion.Header>  {/*아코디언 제목(result title)*/}
+          <Accordion.Body>{/*아코디언 내용*/}
+            {item.body} {/*결과 내용 - 이미지화 또는 텍스트 */}
+            <button onClick={() => onClickHandler(item.tag)}>solution</button> {/* 솔루션 이동 버튼 */}
           </Accordion.Body>
         </Accordion.Item>
       ))}
